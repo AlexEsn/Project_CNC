8bb2e7d7cc4108d5d93f69866e1790acb3af4541
AlexEsn <saschaesa@gmail.com>
2021-11-17 15:25:37 +0300

    add presentation + shema

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "IFIM"
+#define CUSTOM_MACHINE_NAME "NeverMore"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
